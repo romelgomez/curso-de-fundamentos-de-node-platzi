91ff2e2872bbcd63eabbe938ef1e18778562c7a4
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-01-11 10:08:27 -0500

    executing c++ modules in node

---
+++ native/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/hello.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/romel/.cache/node-gyp/12.4.0/include/node -I/home/romel/.cache/node-gyp/12.4.0/src -I/home/romel/.cache/node-gyp/12.4.0/deps/openssl/config -I/home/romel/.cache/node-gyp/12.4.0/deps/openssl/openssl/include -I/home/romel/.cache/node-gyp/12.4.0/deps/uv/include -I/home/romel/.cache/node-gyp/12.4.0/deps/zlib -I/home/romel/.cache/node-gyp/12.4.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c -o Release/obj.target/addon/hello.o ../hello.cc
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/node.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/v8.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/v8-internal.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/v8-version.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/v8config.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/v8-platform.h \
+ /home/romel/.cache/node-gyp/12.4.0/include/node/node_version.h
+../hello.cc:
+/home/romel/.cache/node-gyp/12.4.0/include/node/node.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/v8.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/v8-internal.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/v8-version.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/v8config.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/v8-platform.h:
+/home/romel/.cache/node-gyp/12.4.0/include/node/node_version.h:

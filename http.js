09ae1045c86264c5c833a28a08700a6590f96415
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-01-11 11:29:46 -0500

    http

---
+++ http.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const router = (req, res) => {
+  switch (req.url) {
+    case "/hola":
+      res.writeHead(201);
+      res.write("Hello Word!");
+      break;
+    default:
+      res.writeHead(201);
+      res.write("I know this, trust me!");
+      break;
+  }
+
+  res.end();
+};
+
+http.createServer(router).listen(3000);
+
+console.log("listening in the :3000 port");

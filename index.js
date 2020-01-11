d3bb5105f1bee69c094871979df3766675c7adda
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2020-01-11 09:00:47 -0500

    add example with exec and spawn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 
 exec("ls -ls", (err, stdout) => {
+  console.log("\nExec example:\n");
+
   if (err) {
     console.log(err);
     return false;
   }
   console.log(stdout);
 });
+
+let process = spawn("ls", ["-la"]);
+process.stdout.on("data", data => console.log(data.toString()));
+process.on("exit", () =>
+  console.log("process end; process.killed:", process.killed)
+);
